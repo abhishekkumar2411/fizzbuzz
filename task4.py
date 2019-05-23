bd0f5c31f3e656473fcd44c918f68656ebadb163
[yaoruohan] <[rh918059@dal.ca]>
2019-05-23 11:55:41 -0300

    remove the limit from get-network-hosts.py and have to give the ip address and url to access the controller

--- task4.py
+++ task4.py
@@ -8,3 +8,15 @@ controller='sandboxapic.cisco.com'
 #given that controller='devnetapi.cisco.com/sandbox/apic_em'
 
 
+# put the ip address of your apic-em controller 
+url = "https://" + controller + "/api/v1/ticket"
+
+#type the username and password to access the Controller
+payload = {"username":"devnetuser","password":"Cisco123!"}
+
+header = {"content-type": "application/json"}
+
+response= requests.post(url,data=json.dumps(payload), headers=header, verify=False)
+
+#conversion
+r_json=response.json()
