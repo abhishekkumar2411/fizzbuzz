94b10172c4974bda81ee38406f35fb15bd9afec9
[yaoruohan] <[rh918059@dal.ca]>
2019-05-23 12:01:12 -0300

    Task 4 attempted

--- task4.py
+++ task4.py
@@ -1,22 +1,49 @@
-# import requests libraries
 import requests
 
-#import json libraries
 import json
 
 controller='sandboxapic.cisco.com'
-#given that controller='devnetapi.cisco.com/sandbox/apic_em'
 
 
-# put the ip address of your apic-em controller 
-url = "https://" + controller + "/api/v1/ticket"
+url = "https://" + controller + "/api/v1/tckt"
 
-#type the username and password to access the Controller
-payload = {"username":"devnetuser","password":"Cisco123!"}
+Payload = {"username":"devnetuser","password":"Cisco123!"}
 
-header = {"content-type": "application/json"}
+Headr = {"content-type": "application/json"}
 
-response= requests.post(url,data=json.dumps(payload), headers=header, verify=False)
+rspns= requests.post(url,data=json.dumps(Payload), Headrs=Headr, verify=False)
 
-#conversion
-r_json=response.json()
+#convert rspns to json format
+r_json=rspns.json()
+
+#print(r_json)
+#parse the json to get the service tckt
+tckt = r_json["rspns"]["servicetckt"]
+
+# URL for Host REST API call to get list of exisitng hosts on the network.
+url = "https://" + controller + "/api/v1/host"
+
+#Content type must be included in the Headr as well as the tckt
+Headr = {"content-type": "application/json", "X-Auth-Token":tckt}
+
+# this statement performs a GET on the specified host url
+rspns = requests.get(url, Headrs=Headr, verify=False)
+
+# json.dumps serializes the json into a string and allows us to
+# print the rspns in a 'pretty' format with indentation etc.
+
+
+# Taken Id as name
+print ("Hosts = ")
+print (json.dumps(rspns.json(), indent=4, separators=(',', ': ')))
+
+r_resp=rspns.json()
+host_dic={}
+id_list=[]
+for item in r_resp['rspns']:
+     id_list.append(item['id'])
+     tup=(item['id'],item['hostIp'])
+     host_dic[tup]=item['hostMac']
+     
+print("The Host details are::::",host_dic)   
+
