bc9773670682c01984e2144f72f56d1b154b93f0
[yaoruohan] <[rh918059@dal.ca]>
2019-05-23 11:50:41 -0300

    used predefined functions import requests and import json

---
+++ task4.py
@@ -0,0 +1,10 @@
+# import requests libraries
+import requests
+
+#import json libraries
+import json
+
+controller='sandboxapic.cisco.com'
+#given that controller='devnetapi.cisco.com/sandbox/apic_em'
+
+
